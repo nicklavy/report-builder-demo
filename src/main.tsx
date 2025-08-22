7a2db37274703074a3648d260391810a7d601db4
nicklavy82 <nicklavender.realtimeres@gmail.com>
2025-08-22 08:17:32 -0500

    Tweak: removed chinese characters

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@ import ReactDOM from "react-dom/client";
 import { ConfigProvider, theme } from "antd";
 import "antd/dist/reset.css";   // keep this BEFORE your own CSS
 import "./index.css";
+import en_US from 'antd/lib/locale/en_US';
 import App from "./App";
-
 // Optional: Tailwind color import (works if using ESM)
 import colors from "tailwindcss/colors.js";
 const PRIMARY = colors?.violet?.[600] ?? "#665F9E"; // or "#722ed1" for AntD purple-6
@@ -14,6 +14,7 @@ const PRIMARY = colors?.violet?.[600] ?? "#665F9E"; // or "#722ed1" for AntD pur
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConfigProvider
+      locale={en_US}
       theme={{
         algorithm: theme.defaultAlgorithm,
         token: {
